eb5da7c07b9dbf2c275d3b0e0a3ed7d96fdec52a
henry <zizimoos@gmail.com>
2020-06-10 03:30:59 +0900

    scss

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 
 app.use(localMiddlewares);
 app.use(routes.home, globalRouter);
---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+import "../scss/styles.scss";
+
+const something = async () => {
+  await console.log("core-js or babel-polyfill");
+};
